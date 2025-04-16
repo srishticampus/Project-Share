6e0d973a0a094fd74c71f13c8009931c8268a90d
Arjun S <arjuns.sics@gmail.com>
2025-04-16 13:19:52 +0530

    init

---
+++ client/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import { Button } from "./components/ui/button";
+import { Route, Routes } from "react-router";
+function Home() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <Button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </Button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
+}
+
+export default App;
