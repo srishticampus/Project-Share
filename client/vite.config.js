0187fe8191e167a25d6eb42b4fdd69142d323771
Jithin Jose <007jithinjose@gmail.com>
2025-05-27 21:46:37 +0530

    changed base name in vite config of client

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,23 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
+
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: process.env.VITE_BASE_URL,
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory
+  const env = loadEnv(mode, process.cwd(), "");
+  
+  return {
+    plugins: [react(), tailwindcss()],
+    base: env.VITE_BASE_URL || "/project_share/", // Fallback if env var not set
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
+    server: {
+      open: env.VITE_BASE_URL || "/project_share/", // Open browser at correct path
+    },
+  };
 });
